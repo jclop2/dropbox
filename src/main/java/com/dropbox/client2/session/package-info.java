03a07a218deadf335138eedd5b0d210455616925
Fathzer <jm-dev@astesana.net>
2023-02-13 19:46:42 +0100

    Fixes library dependency not compatible with Java 1.6

--- src/main/java/com/dropbox/client2/session/package-info.java
+++
@@ -1,5 +0,0 @@
-/** This package contains previous classes from Dropbox API which have been removed in new version
- * and are required to preserve compatibility. 
- * @author Jean-Marc Astesana (of course, greatly inspired by old Dropbox core API implementation)
- */
-package com.dropbox.client2.session;
